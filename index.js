8b1242055264907f14d96401f105d0eb1f23fc1d
ogzacky75 <ogzacky75@gmail.com>
2025-07-01 12:49:23 +0300

    dodger

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function moveDodgerLeft(){
+
+}
+
+function moveDodgerRight(){
+
+}
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
